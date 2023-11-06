b38cdd55a0e22abb42de2fbf76475ad0508cf931
sofiiakryvushko <sofiiakryvushko@hmail.com>
2023-11-06 17:15:54 -0600

    update-favorite

--- app/api/galleries_favorites_routes.py
+++ app/api/galleries_favorites_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request
-from app.models import db, GalleryFavorite
+from app.models import db, GalleryFavorite, Gallery
 from flask_login import current_user, login_required
 
 gallery_favorite_routes = Blueprint('galleries_favorites', __name__)
@@ -32,3 +32,23 @@ def add_gallery_to_favorites(gallery_id):
         db.session.add(new_favorite)
         db.session.commit()
         return {'message': 'Gallery added to favorites'}, 201
+
+@gallery_favorite_routes.route('/<int:favorite_id>', methods=['PUT'])
+@login_required
+def update_gallery_status(favorite_id):
+    user_id = current_user.id
+
+    favorite = GalleryFavorite.query.filter_by(id=favorite_id, user_id=user_id).first()
+    if favorite:
+        gallery_id = favorite.gallery_id
+        gallery = Gallery.query.get(gallery_id)
+
+        if gallery:
+            # Toggle the status: if it's true, make it false; if it's false, make it true
+            gallery.status = not gallery.status
+            db.session.commit()
+            return {'message': 'Gallery status updated successfully'}, 200
+        else:
+            return {'message': 'Gallery not found'}, 404
+    else:
+        return {'message': 'Gallery favorite not found'}, 404
