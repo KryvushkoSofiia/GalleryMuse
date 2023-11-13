074c04518b8fb7db19f871217884451e7c3f0ef5
sofiiakryvushko <sofiiakryvushko@hmail.com>
2023-11-12 22:09:25 -0600

    seeder model changes

---
+++ app/models/visited_galleries.py
@@ -0,0 +1,22 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class VisitedGallery(db.Model):
+    __tablename__='visited_galleries'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')))
+    gallery_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('galleries.id')))
+
+    user = db.relationship('User', back_populates='visited_galleries')
+    gallery = db.relationship('Gallery', back_populates='visited_by')
+
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'gallery_id': self.gallery_id
+        }
---
+++ app/seeds/visited_galleries.py
@@ -0,0 +1,24 @@
+# from app.models import db, User, Gallery, VisitedGallery, environment, SCHEMA
+# from sqlalchemy.sql import text
+# import random
+
+# def seed_visited_galleries(num_visited=40):
+#     user_ids = [user.id for user in User.query.all()]
+#     gallery_ids = [gallery.id for gallery in Gallery.query.all()]
+
+#     for _ in range(num_visited):
+#         user_id = random.choice(user_ids)
+#         gallery_id = random.choice(gallery_ids)
+
+#         visited = VisitedGallery(user_id=user_id, gallery_id=gallery_id)
+#         db.session.add(visited)
+
+#     db.session.commit()
+
+# def undo_visited_galleries():
+#     if environment == "production":
+#         db.session.execute(f"TRUNCATE table {SCHEMA}.visited_galleries RESTART IDENTITY CASCADE;")
+#     else:
+#         db.session.execute(text("DELETE FROM visited_galleries"))
+
+#     db.session.commit()
