11a47d450c62314686a0c082729bea6f44bc9a56
sofiiakryvushko <sofiiakryvushko@hmail.com>
2024-02-18 20:08:35 -0600

    footer changes

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -1,48 +1,47 @@
-// import React from "react";
-// import { NavLink } from "react-router-dom";
-// import { useSelector } from "react-redux";
-// import "./Footer.css"; // Import your CSS file here
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import "./Footer.css"; // Import your CSS file here
 
-// function Footer({ isLoaded }) {
-//     const sessionUser = useSelector((state) => state.session.user);
+function Footer({ isLoaded }) {
+    const sessionUser = useSelector((state) => state.session.user);
 
-//     const technologies = ["Python", "Flask", "React", "Redux", "HTML", "CSS"];
+    const technologies = ["Python", "Flask", "React", "Redux", "HTML", "CSS"];
 
-//     return (
-//         // <div className="footer">
-//         //     <div className="column">
-//         //         <NavLink exact to="/">
-//         //             {/* <img className="logo" src={logo} alt="Logo" /> */}
-//         //         </NavLink>
-//         //         <NavLink to="/">Home</NavLink>
-//         //         {sessionUser && (
-//         //             <>
-//         //                 <NavLink to="/create-gallery">Create New Gallery</NavLink>
-//         //                 <NavLink to="/my-galleries">My Galleries</NavLink>
-//         //             </>
-//         //         )}
-//         //     </div>
+    return (
+        <div className="footer">
+            <div className="column">
+                <NavLink exact to="/">
+                    {/* <img className="logo" src={logo} alt="Logo" /> */}
+                </NavLink>
+                <NavLink to="/">Home</NavLink>
+                {sessionUser && (
+                    <>
+                        <NavLink to="/create-gallery">Create New Gallery</NavLink>
+                        <NavLink to="/my-galleries">My Galleries</NavLink>
+                    </>
+                )}
+            </div>
 
-//         //     <div className="column">
-//         //         <p>Developers Github</p>
-//         //         <a href="https://github.com/KryvushkoSofiia" target="_blank" rel="noopener noreferrer">
-//         //             Sofiia Kryvushko
-//         //         </a>
-//         //     </div>
+            <div className="column">
+                <p>Developers Github</p>
+                <a href="https://github.com/KryvushkoSofiia" target="_blank" rel="noopener noreferrer">
+                    Sofiia Kryvushko
+                </a>
+            </div>
 
-//         //     <div className="column">
-//         //         <p>Technologies Used</p>
-//         //         <ul>
-//         //             {technologies.map((tech, index) => (
-//         //                 <li key={index}>{tech}</li>
-//         //             ))}
-//         //         </ul>
-//         //     </div>
+            <div className="column">
+                <p>Technologies Used</p>
+                <ul>
+                    {technologies.map((tech, index) => (
+                        <li key={index}>{tech}</li>
+                    ))}
+                </ul>
+            </div>
 
-//         //     {/* {isLoaded && <ProfileButton user={sessionUser} />} */}
-//         // </div>
-//         null
-//     );
-// }
+            {/* {isLoaded && <ProfileButton user={sessionUser} />} */}
+        </div>
+    );
+}
 
-// export default Footer;
+export default Footer;
